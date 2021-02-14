43a9ba5e6ff22451e69e4798192f2a62588514fb
FaziletKosure <faziletkosure1@gmail.com>
2021-02-14 20:45:51 +0100

    added getStaticPaths()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -16,16 +16,44 @@ const  article= ({article}) => {
     )
 }
 
-export const getServerSideProps=async (context)=>{
-    const res= await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+// export const getServerSideProps=async (context)=>{
+//     const res= await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
 
-    const article=await res.json()
+//     const article=await res.json()
 
-    return{
-        props:{
-            article
-        }
-    }
+//     return{
+//         props:{
+//             article
+//         }
+//     }
+// }
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+  const articles = await res.json()
+
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export default article
